3b3f05aa82a77912b1499ec349e1d0f587d933fc
devalgarg129 <devalgarg978@gmail.com>
2026-05-26 12:50:48 +0530

    containerzing detectxr

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,26 @@
 #include <drogon/drogon.h>
+#include <drogon/orm/DbConfig.h>
+
 #include "controllers/SubmissionController.h"
 
 int main() {
+    drogon::orm::PostgresConfig dbConfig;
+    dbConfig.host = "postgres";
+    dbConfig.port = 5432;
+    dbConfig.databaseName = "detectxr";
+    dbConfig.username = "detectxr_user";
+    dbConfig.password = "strongpassword";
+    dbConfig.connectionNumber = 1;
+    dbConfig.name = "postgresql";
+    dbConfig.isFast = false;
+    dbConfig.characterSet = "utf8";
+    dbConfig.timeout = 1.0;
+    dbConfig.autoBatch = false;
 
     drogon::app()
+        .addDbClient(dbConfig)
         .addListener("0.0.0.0", 8080)
         .run();
+
     return 0;
 }
